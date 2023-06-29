8708fff7af1175c4814af65f0ec9d9ae22474d64
Cyber Secrets - Red Team github <infosecwriter@gmail.com>
2023-06-29 15:29:59 -0600

    Update csitoolsupdate22.sh

--- csitoolsupdate22.sh
+++ csitoolsupdate22.sh
@@ -38,8 +38,13 @@ sudo apt-key del 76F1A20FF987672F
 sudo apt-key del 750179FCEA62
 echo $key | sudo -S rm -rf /etc/apt/sources.list.d/archive_u*
 
+
+
+
+echo $key | sudo -S sudo curl -fsSL https://download.bell-sw.com/pki/GPG-KEY-bellsoft | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/bellsoft.gpg >/dev/null
+echo $key | sudo -S bash -c "echo 'deb [arch=amd64] https://apt.bell-sw.com/ stable main' | tee /etc/apt/sources.list.d/bellsoft.list"
 echo $key | sudo -S sudo curl -fsSL https://apt.vulns.sexy/kpcyrd.pgp | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/apt-vulns-sexy.gpg >/dev/null
-echo $key | sudo -S bash -c "echo 'deb http://apt.vulns.sexy stable main' | tee /etc/apt/sources.list.d/wine.list"
+echo $key | sudo -S bash -c "echo 'deb http://apt.vulns.sexy stable main' | tee /etc/apt/sources.list.d/apt-vulns-sexy.list"
 echo $key | sudo -S sudo curl -fsSL https://dl.winehq.org/wine-builds/winehq.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/winehq.gpg >/dev/null
 echo $key | sudo -S bash -c "echo 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main' | tee /etc/apt/sources.list.d/wine.list"
 echo $key | sudo -S sudo curl -fsSL https://www.kismetwireless.net/repos/kismet-release.gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/kismet-release.gpg >/dev/null
