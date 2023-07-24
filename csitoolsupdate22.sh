0042f27ef4463f1a8b864f0ef8f84eaef10a8c62
Cyber Secrets - Red Team github <infosecwriter@gmail.com>
2023-07-24 11:33:31 -0600

    Update csitoolsupdate22.sh

--- csitoolsupdate22.sh
+++ csitoolsupdate22.sh
@@ -34,6 +34,7 @@ rm /opt/csitools/helper/sn0int
 rm /opt/csitools/helper/exif
 echo $key | sudo -S mkdir /iso
 echo $key | sudo -S chown csi:csi /iso -R
+tar -xf /opt/csitools/assets/Win11-blue.tar.xz --directory /home/csi/.icons/
 
 echo $key | sudo -S /bin/sed -i 's/http\:\/\/in./http\:\/\//g' /etc/apt/sources.list
 echo $key | sudo -S echo "\$nrconf{restart} = 'a'" | sudo -S tee /etc/needrestart/conf.d/autorestart.conf > /dev/null
