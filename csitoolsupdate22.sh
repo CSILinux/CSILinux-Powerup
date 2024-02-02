ecfb8e667e584d2c764b1d5480925de32a967003
Cyber Secrets - Red Team github <infosecwriter@gmail.com>
2024-02-02 14:45:59 -0700

    Update csitoolsupdate22.sh
    Cleaning up the upgrade

--- csitoolsupdate22.sh
+++ csitoolsupdate22.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 echo "Powerup has been updated.  This will update CSI Tools with the newest updated powerup"
+read "Press Enter to continue"
 echo "Installing CSI Linux Tools and Menu update"
 rm csi* > /dev/null 2>&1
 echo "Downloading CSI Tools"
-wget https://csilinux.com/download/csitools.zip -O csitools.zip
+wget https://csilinux.com/download/csitools22.zip -O csitools22.zip
 echo "# Installing CSI Tools"
-echo $key | sudo -S unzip -o -d / csitools.zip > /dev/null 2>&1
+echo $key | sudo -S unzip -o -d / csitools22.zip > /dev/null 2>&1
 echo $key | sudo -S chown csi:csi -R /opt/csitools  > /dev/null 2>&1
 echo $key | sudo -S chmod +x /opt/csitools/* -R > /dev/null 2>&1
 echo $key | sudo -S chmod +x /opt/csitools/* > /dev/null 2>&1
@@ -15,5 +16,4 @@ echo $key | sudo -S ln -sf /opt/csitools/powerup /usr/local/bin/powerup > /dev/n
 echo $key | sudo -S mkdir /iso > /dev/null 2>&1
 
 echo "The powerup will run again to finish they upgrade"
-pause
 /opt/csitools/powerup
