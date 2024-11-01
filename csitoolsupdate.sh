f25aaa8485399ed9f7a299b82c815a07c9814a77
Cyber Secrets - Red Team github <infosecwriter@gmail.com>
2024-11-01 15:02:56 -0600

    update

--- csitoolsupdate.sh
+++ csitoolsupdate.sh
@@ -1428,7 +1428,7 @@ Categories=Finance;Network;" > ~/.local/share/applications/OxenWallet.desktop
 		echo $key | sudo -S systemctl enable libvirtd
     		sudo -k
 		;;
-        "security")
+        "security2")
 		echo "Setting up Security tools..."
 		cd /tmp
 		rm csi_security.txt &>/dev/null
